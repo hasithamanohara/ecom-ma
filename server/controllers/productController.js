2c264adf1be3d3f14fe7c9d94d3b548bad7e9e57
Hasith manohara <manoharauh.20@uom.lk>
2024-06-29 23:10:44 +0530

    product controller done

---
+++ server/controllers/productController.js
@@ -0,0 +1,60 @@
+const product = require("../models/product");
+const product = require("../models/product");
+const product = require("../models/product");
+const product = require("../models/product");
+const product = require("../models/product");
+const product = require("../models/product");
+
+module.exports = {
+  createProduct: async (req, res) => {
+    const newProduct = new product(req.body);
+    try {
+      await newProduct.save();
+      res.status(200).json("product created successfully");
+    } catch (e) {
+      res.status(500).json(e + " failed to product create");
+    }
+  },
+
+  getAllProducts: async (req, res) => {
+    try {
+      const products = await product.find().sort({ createdAt: -1 });
+      res.status(200).json(products);
+    } catch (e) {
+      res.status(500).json(e + " failed to get all products");
+    }
+  },
+
+  getProductByID: async (req, res) => {
+    const productId = req.params.product;
+    try {
+      const product = await product.findByID(productId);
+      const { _v, createdAt, ...productData } = product._doc;
+      res.status(200).json(productData);
+    } catch (e) {
+      res.status(500).json("Product not found");
+    }
+  },
+
+  searchProduct: async (req, res) => {
+    const query = req.query.product;
+    try {
+      const result = await product.aggregate([
+        {
+          $search: {
+            index: "shoes",
+            text: {
+              query: req.params.key,
+              path: {
+                wildcard: "*",
+              },
+            },
+          },
+        },
+      ]);
+      res.status(200).json(result);
+    } catch (e) {
+      res.status(500).json(e + " failed to get the products");
+    }
+  },
+};
